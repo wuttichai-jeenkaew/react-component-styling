aacea447ab084b73e8a05e5330566a0c2853f869
Mind Sathirasrindh <109655871+mydjolie@users.noreply.github.com>
2023-02-01 15:30:52 +0700

    feat:setting up vite project

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <div className="button-components-section">
+        {/* ถ้าสร้าง Component ของ Buttons เสร็จแล้วให้ Uncomment โค้ดด้านล่าง เพื่อดูผลลัพธ์ */}
+        {/* <Button color="primary">Primary</Button>
+        <Button color="secondary">Secondary</Button> */}
+      </div>
+      <hr />
+      <div className="alert-components-section">
+        {/* ถ้าสร้าง Component Alert เสร็จแล้วให้ Uncomment โค้ดด้านล่าง เพื่อดูผลลัพธ์ */}
+        {/*
+          <Alert severity="error">This is error alert box</Alert>
+          <Alert severity="warning">This is a warning alert box</Alert>
+          <Alert severity="info">This is an info alert box</Alert>
+          <Alert severity="success">This is a success alert box</Alert>
+        */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
