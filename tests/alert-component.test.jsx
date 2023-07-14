2d408c1d45f54b6d940381e7744b1852592fbd39
Baimorn <baimorn55@gmail.com>
2023-07-14 13:13:52 +0700

    feat: edit test title

--- tests/alert-component.test.jsx
+++ tests/alert-component.test.jsx
@@ -3,50 +3,50 @@ import { expect } from "vitest";
 import { Alert } from "../src/components/Alert.jsx"
 
 describe('Component Styling : Alert', () => {
-    
-    it('renders error alert correctly', () => {
-        render(<Alert severity="error">This is an error alert — check it out!</Alert>);
-        const alertElement = screen.getByText('This is an error alert — check it out!');
 
-        expect(alertElement).toBeInTheDocument();
-        expect(alertElement).toHaveStyle(`
+  it('Render ตัว Component Alert แบบทั่วไปได้ถูกต้อง', () => {
+    render(<Alert severity="error">This is an error alert — check it out!</Alert>);
+    const alertElement = screen.getByText('This is an error alert — check it out!');
+
+    expect(alertElement).toBeInTheDocument();
+    expect(alertElement).toHaveStyle(`
           background-color: #F9C8C8;
         `);
-        
-    });
 
-    it('renders warning alert correctly', () => {
-        render(<Alert severity="warning">This is a warning alert — check it out!</Alert>);
-        const alertElement = screen.getByText('This is a warning alert — check it out!');
+  });
 
-        expect(alertElement).toBeInTheDocument();
-        expect(alertElement).toHaveStyle(`
+  it('Render ตัว Alert แบบ "warning" ได้ถูกต้อง', () => {
+    render(<Alert severity="warning">This is a warning alert — check it out!</Alert>);
+    const alertElement = screen.getByText('This is a warning alert — check it out!');
+
+    expect(alertElement).toBeInTheDocument();
+    expect(alertElement).toHaveStyle(`
           background-color: #F9D9C8;
         `);
-        
-    });
 
-    it('renders info alert correctly', () => {
-        render(<Alert severity="info">This is an info alert — check it out!</Alert>);
-        const alertElement = screen.getByText('This is an info alert — check it out!');
+  });
 
-        expect(alertElement).toBeInTheDocument();
-        expect(alertElement).toHaveStyle(`
+  it('Render ตัว Alert แบบ "info" ได้ถูกต้อง', () => {
+    render(<Alert severity="info">This is an info alert — check it out!</Alert>);
+    const alertElement = screen.getByText('This is an info alert — check it out!');
+
+    expect(alertElement).toBeInTheDocument();
+    expect(alertElement).toHaveStyle(`
           background-color: #F9EBC8;
         `);
-        
-    });
 
-    it('renders success alert correctly', () => {
-        render(<Alert severity="success">This is a success alert — check it out!</Alert>);
-        const alertElement = screen.getByText('This is a success alert — check it out!');
+  });
 
-        expect(alertElement).toBeInTheDocument();
-        expect(alertElement).toHaveStyle(`
+  it('Render ตัว Alert แบบ "success" ได้ถูกต้อง', () => {
+    render(<Alert severity="success">This is a success alert — check it out!</Alert>);
+    const alertElement = screen.getByText('This is a success alert — check it out!');
+
+    expect(alertElement).toBeInTheDocument();
+    expect(alertElement).toHaveStyle(`
           background-color: #CEF7CD;
         `);
-        
-    });
+
+  });
 
 
 });
--- tests/button-component.test.jsx
+++ tests/button-component.test.jsx
@@ -5,27 +5,27 @@ import { Button } from "../src/components/Button.jsx"
 
 
 describe('Component Styling : Button', () => {
-    it('renders primary button correctly', () => {
-        render(<Button color="primary">Primary</Button>);
-        const buttonElement = screen.getByRole('button');
+  it('Render ตัว Button แบบ "primary" ได้ถูกต้อง', () => {
+    render(<Button color="primary">Primary</Button>);
+    const buttonElement = screen.getByRole('button');
 
-        expect(buttonElement).toBeInTheDocument();
-        expect(buttonElement).toHaveStyle(`
+    expect(buttonElement).toBeInTheDocument();
+    expect(buttonElement).toHaveStyle(`
           background-color: #074EE8;
         `);
 
-    })
+  })
 
-    it('renders secondary button correctly', () => {
-        render(<Button color="secondary">Secondary</Button>);
-        const buttonElement = screen.getByRole('button');
+  it('Render ตัว Button แบบ "secondary" ได้ถูกต้อง', () => {
+    render(<Button color="secondary">Secondary</Button>);
+    const buttonElement = screen.getByRole('button');
 
-        expect(buttonElement).toBeInTheDocument();
-        expect(buttonElement).toHaveStyle(`         
+    expect(buttonElement).toBeInTheDocument();
+    expect(buttonElement).toHaveStyle(`         
           background-color: #07A4E8;
         `);
 
-    });
+  });
 
 });
 
